dcaeeb8aa640a7da2471dd907a30b0d1360514d1
kjmin-dev <kjmin.kr@gmail.com>
2024-09-22 15:36:37 +0900

    add: Init docker-compose for swagger

--- apps/service1-nest/src/main.ts
+++ apps/service1-nest/src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const config = new DocumentBuilder()
     .setTitle('Service1')
     .setDescription('Service1 API description')
@@ -12,8 +12,9 @@ async function bootstrap() {
     .addTag('Service1')
     .build();
   const document = SwaggerModule.createDocument(app, config);
+  app.enableCors();
   SwaggerModule.setup('api-docs', app, document, {
-    jsonDocumentUrl: 'swagger/json',
+    jsonDocumentUrl: 'api-docs/json',
   });
   await app.listen(3000);
 }
--- apps/service2-nest/src/main.ts
+++ apps/service2-nest/src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const config = new DocumentBuilder()
     .setTitle('Service2')
     .setDescription('Service2 API description')
@@ -12,10 +12,11 @@ async function bootstrap() {
     .addTag('Service2')
     .build();
   const document = SwaggerModule.createDocument(app, config);
+  app.enableCors();
   SwaggerModule.setup('api-docs', app, document, {
-    jsonDocumentUrl: 'swagger/json',
+    jsonDocumentUrl: 'api-docs/json',
   });
-  await app.listen(3000);
+  await app.listen(3010);
 }
 
 bootstrap();
